6102ef1a7ace8c00abcbdbd4d5d2df7c1fecbcec
MaggieDeSantis <sheisntreal@gmail.com>
2023-03-11 11:40:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards (names, event) {
+    let messages=[]
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+console.log ["Guadalupe", "Ollie", "Aki"] ;
+console.log (writeCards(namems));
+
+
+function countDown(number) {
+    let value= number
+    while (value >=0) {
+        console.log(value--);
+    }
+    return number;
+}
